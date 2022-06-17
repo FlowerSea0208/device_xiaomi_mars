e193df7dcf5a0e6e2e7bbc3c457a483218e5fc30
FlowerSea0208 <zzp060208@gmail.com>
2022-06-17 21:44:15 +0800

    mars: First Snow (EvoX 6.0)

--- evolution_mars.mk
+++ evolution_mars.mk
@@ -14,16 +14,17 @@ $(call inherit-product, vendor/xiaomi/mars-miuicamera/products/miuicamera.mk)
 # Inherit from mars device
 $(call inherit-product, device/xiaomi/mars/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+EVO_BUILD_TYPE := UNOFFICIAL
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
+EXTRA_UDFPS_ANIMATIONS := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := mars
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2102K1AC
-PRODUCT_NAME := aosp_mars
+PRODUCT_NAME := evolution_mars
 
 TARGET_BOOT_ANIMATION_RES := 1440
 
